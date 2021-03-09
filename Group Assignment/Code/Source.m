d6869f9c2091653ff8aabc5f5550eabc2c4ef838
RSuryaNarayan <52670594+RSuryaNarayan@users.noreply.github.com>
2021-03-09 17:13:44 +0530

    Create Source.m

---
+++ Group Assignment/Code/Source.m
@@ -0,0 +1,16 @@
+%cfdassy: FDM porous fin 
+mesh=11;
+t=zeros(1,mesh);
+t(end)=1;
+dx=1/(mesh-1);
+w1=1.1422;w2=0.5710;
+for k=1:100
+    %t_old=t;
+    for i=2:mesh-1
+        t(i)=(t(i+1)+t(i-1)-w1*(dx^2)*(t(i))^2)/(2+w2*dx^2);
+    end
+    t(1)=t(2);
+end
+x=linspace(0,1,mesh);
+plot(x,t,'*r');hold on;%put analytical here
+plot(x,t,'-g');hold off;
