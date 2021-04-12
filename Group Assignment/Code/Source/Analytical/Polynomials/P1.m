b7072cbe692e86bd9881f01092f8c182f9885157
RSuryaNarayan <52670594+RSuryaNarayan@users.noreply.github.com>
2021-04-12 18:26:28 +0530

    added polynomials for adomain decomposition

---
+++ Group Assignment/Code/Source/Analytical/Polynomials/P1.m
@@ -0,0 +1,3 @@
+function ans = P1(w1,w2,t0)
+    ans = (w1*t0^2 + w2*t0)/2 
+end
---
+++ Group Assignment/Code/Source/Analytical/Polynomials/P2.m
@@ -0,0 +1,3 @@
+function ans = P2(w1,w2,t0)
+    ans = (2 * w1^2 * t0^3 + 3 * w1* w2 * t0^2 + w2^2 * t0)/4/3/2/1
+end
